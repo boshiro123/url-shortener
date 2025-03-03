568709c72f0ef22fbe6e0864e30e7eb444274748
boshiro123 <b.shiroki@mail.ru>
2025-03-04 01:48:25 +0300

    add config

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,13 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"urlshortener/internal/config"
+)
+
 func main() {
+
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
 	// TODO: init config: cleanenv
 
 	// TODO: init logger: slog
 
-	// TODO: init storage: 
+	// TODO: init storage: sqlite
 
-	// TODO: init router
+	// TODO: init router: chi, "chi render"
 
-	// TODO: run server
+	// TODO: run server:
 }
